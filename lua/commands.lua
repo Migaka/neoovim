639b57dc8fbe5ab537047e64ac39572df073981d
Miho Migaka <MigakaAkatta@hotmail.com>
2022-02-09 09:05:33 -0300

    Adding default options and removing old file

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,5 +1,21 @@
 local options = {
-
+    syntax = "on",
+    fileencoding = "utf-8",
+    visualbell = true,
+    ruler = true,
+    wrap = true,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    incsearch = true,
+    autoindent = true,
+    smartindent = true,
+    number = true,
+    swapfile = false,
+    clipboard = "unnamedplus",
+    laststatus = 2,
+    showmode = off
 }
 
 for k, v in pairs(options) do
