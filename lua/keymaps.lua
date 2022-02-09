c05a5eb6f59d3d75d297669fc4b5ee0f758306ad
Miho Migaka <MigakaAkatta@hotmail.com>
2022-02-09 20:06:18 -0300

    Removing useles code

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,14 +1,3 @@
-local Utils = require('utils')
-
--- local exprnnoremap = Utils.exprnnoremap
-local nnoremap = Utils.nnoremap
-local vnoremap = Utils.vnoremap
--- local xnoremap = Utils.xnoremap
-local inoremap = Utils.inoremap
--- local tnoremap = Utils.tnoremap
--- local nmap = Utils.nmap
--- local xmap = Utils.xmap
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
