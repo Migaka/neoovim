5c0e6e409afdb10d17ec5aa627c76283f890cbb3
Miho Migaka <MigakaAkatta@hotmail.com>
2022-02-09 09:38:17 -0300

    Add keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,50 @@
+local Utils = require('utils')
+
+-- local exprnnoremap = Utils.exprnnoremap
+local nnoremap = Utils.nnoremap
+local vnoremap = Utils.vnoremap
+-- local xnoremap = Utils.xnoremap
+local inoremap = Utils.inoremap
+-- local tnoremap = Utils.tnoremap
+-- local nmap = Utils.nmap
+-- local xmap = Utils.xmap
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- kj to normal mode
+inoremap("kj", "<Esc>")
+
+-- Save with Ctrl + S
+nnoremap("<C-s>", ":w<CR>")
+
+-- Close buffer
+nnoremap("<C-c>", ":q<CR>")
+
+-- Move around windows
+nnoremap("<C-h>", "<C-w>h")
+nnoremap("<C-j>", "<C-w>j")
+nnoremap("<C-k>", "<C-w>k")
+nnoremap("<C-l>", "<C-w>l")
+
+-- Splits
+nnoremap("<leader>ws", ":split<CR>")
+nnoremap("<leader>vs", ":vsplit<CR>")
+
+-- Delete buffer
+nnoremap("<A-w>", ":bd<CR>")
+
+-- Yank to end of line
+nnoremap("Y", "y$")
+
+-- Copy to system clippboard
+nnoremap("<leader>y", '"+y')
+vnoremap("<leader>y", '"+y')
+
+-- Paste from system clippboard
+nnoremap("<leader>p", '"+p')
+vnoremap("<leader>p", '"+p')
+
+-- Clear highlight search
+nnoremap("<leader>nh", ":nohlsearch<CR>")
+vnoremap("<leader>nh", ":nohlsearch<CR>")
